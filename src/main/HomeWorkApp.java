69b1b57476910d3dcea08e5ac4b2a1358917fb23
mahalekk <arman_beimishev@mail.ru>
2021-11-27 12:13:36 +0600

    add files

---
+++ src/main/HomeWorkApp.java
@@ -0,0 +1,45 @@
+package main;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    static void printThreeWords() {
+        System.out.println("Orange\n" + "Banana\n" + "Apple");
+    }
+
+    static void checkSumSign() {
+        int a = -5;
+        int b = 2;
+        if ((a + b) >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    static void printColor() {
+        int value = 1002;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        } else if (value > 100) {
+            System.out.println("Зеленый");
+        }
+    }
+
+    static void compareNumbers() {
+        int a = -50;
+        int b = -102;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+}

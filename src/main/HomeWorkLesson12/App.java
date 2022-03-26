62adbffc21531098444e91996166af7698e4ae37
Arman Beimishev <a.beimishev@sergek.kz>
2022-03-26 17:10:27 +0600

    merge to lesson1

--- src/main/HomeWorkLesson12/App.java
+++ src/main/HomeWorkLesson12/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("WorkController.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("resources/WorkController.fxml"));
         Scene scene = new Scene(fxmlLoader.load ());
         stage.setTitle ("Chat");
         stage.setScene(scene);

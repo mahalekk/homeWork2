56bba2d41e89e7e78fc09484ed305e80599408a1
Arman Beimishev <a.beimishev@sergek.kz>
2022-03-28 21:17:10 +0600

    lesson17

---
+++ src/main/HomeWorkLesson17/Orange.java
@@ -0,0 +1,9 @@
+package main.HomeWorkLesson17;
+
+public class Orange implements Fruit {
+    double weight = 1.5f;
+    @Override
+    public double getWeightFruit() {
+        return this.weight;
+    }
+}
